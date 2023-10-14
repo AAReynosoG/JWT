41bdcd1443ebb65b3a987016bf83c998a7fc507f
Alonso <abrahamreynoso64@gmail.com>
2023-10-13 23:30:19 -0600

    Funcion y endpoint para registrar usuarios

---
+++ database/migrations/2023_10_14_044522_create_usuarios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 200)->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')
+                    ->references('id')
+                    ->on('roles');
+            $table->boolean('is_active')->default(false);                        
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| 
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function() { 
+    return response()->json([
+        'message' => 'Felicidades',
+        'message2' => 'conexion exitosa.'
+        ], 200);
+});
+
+Route::post('/register', [UsuarioController::class, 'register']); /**Ruta para registrar los usuarios */
+Route::get('/activate-account/{userID}');/**Ruta para generar token */
+Route::post('/login');/**Ruta para el login */
+Route::delete('/logout');/**Ruta para deshabilitar el token */
+Route::get('/user/{userID}');/**Ruta para traer la informacion del dueño del token */
